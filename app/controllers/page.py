7ec8eafd0a8b857378d4d5dc92c29c0f12b8b5cd
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-08-20 20:39:43 -0300

    Formulario Flask
    -integração de templates
    -Criação do formulario

--- app/controllers/page.py
+++ app/controllers/page.py
@@ -1,12 +1,16 @@
 from app import app
 from flask import render_template
-
+from app.model.forms import LoguinForm
 
 @app.route("/index/<user>")
 @app.route("/", defaults={"user":None})
 def index(user):
     return  render_template('index.html', user=user)
 
-@app.route("/loguin")
+@app.route("/loguin", methods=["GET","POST"])
 def loguin():
-    return render_template("loguin.html")
+    form = LoguinForm()
+    if form.validate_on_submit():
+        print(form.username.data)
+        print(form.password.data)
+    return render_template("loguin.html", form=form)
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 DEBUG = True
 SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+
+SECRET_KEY = 'chave-secreta-form'

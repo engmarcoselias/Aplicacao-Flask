1331139026cf6a6f61e769fcc4e5e6970d7c002e
Marcos Vinicius Silva Elias <110794069+engmarcoselias@users.noreply.github.com>
2023-08-28 23:43:24 -0300

    Finalizando Aulas
    -Importando flask login
    -Adicionando instancia para login
    -Criando rota
    -Autenticando usuario
    -Teste de usuario logando
    -link para logout

--- app/controllers/page.py
+++ app/controllers/page.py
@@ -1,20 +1,38 @@
 from app import app, db
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.model.forms import LoguinForm
 from app.model.tables import User
+from flask_login import login_user, logout_user
 
 @app.route("/index/<user>")
 @app.route("/", defaults={"user":None})
 def index(user):
     return  render_template('index.html', user=user)
 
+
+#-----------Pagina de loguin-----------------------
 @app.route("/loguin", methods=["GET","POST"])
 def loguin():
     form = LoguinForm()
     if form.validate_on_submit():
-        print(form.username.data)
-        print(form.password.data)
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("Usuario logado")
+            return redirect(url_for("index"))
+        else:
+            flash("Usuario ou senha incorretos!")
+        #print(form.username.data)
+        #print(form.password.data)
     return render_template("loguin.html", form=form)
+
+
+#----------Pagina de Logout----------------------------
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    logout_user()
+    flash("Usuario Saiu")
+    return redirect(url_for("loguin"))
 
 
 #FAZENDO CRUD NO BANCO DE DADOS CREATE, READER, UPDATE, DELETE
--- app/model/tables.py
+++ app/model/tables.py
@@ -1,10 +1,27 @@
-from app import db
+from app import db, lm
+
+@lm.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     password = db.Column(db.String)
     email = db.Column(db.String)
+
+    @property
+    def is_authenticated(self):
+        return True
+    @property
+    def is_active(self):
+        return True
+    @property
+    def is_anonymous(self):
+        return False
+    
+    def get_id(self):
+        return str(self.id)
 
 
     def __init__(self, username, password, email):
